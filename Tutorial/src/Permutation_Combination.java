a69a538b6207f05674a2603f3821e8d051b87ee9
Nebularswamp <grosswamp@gmail.com>
2023-02-20 23:39:08 -0500

    Combination-Permutation

--- Tutorial/src/Permutation_Combination.java
+++ Tutorial/src/Permutation_Combination.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Permutation_Combination {
 
     public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
         System.out.println(Combination(5,1));
+        System.out.println("n= number elements; k=numbers of chosen items");
+        System.out.print("Enter n >");
+        int n = input.nextInt();
+        System.out.print("Enter k >");
+        int k = input.nextInt();
+        System.out.println("combinations: "+Combination(n,k));
+        System.out.println("permutations: "+Permutation(n,k));
     }
 
     public static int Combination(int n,int r){
